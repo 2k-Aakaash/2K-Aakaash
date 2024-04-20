beb08f1c429dc637fdd6a410f8929906505839b9
2K Aakaash <ruokvin434@gmail.com>
2024-04-20 17:45:46 +0530

    Bug Fixed

--- script.js
+++ script.js
@@ -1108,7 +1108,7 @@ __p+='`), he
                 r = document,
                 n = e("html"),
                 i = e("body"),
-                o = ".w-webflow-badge",
+                // o = ".w-webflow-badge",
                 a = window.location,
                 c = /PhantomJS/i.test(navigator.userAgent),
                 u = "fullscreenchange webkitfullscreenchange mozfullscreenchange msfullscreenchange",
@@ -1124,15 +1124,15 @@ __p+='`), he
                 e(f).attr("style", A ? "display: none !important;" : "")
             }
 
-            function h() {
-                var A = e('<a class="w-webflow-badge"></a>').attr("href", "https://webflow.com?utm_campaign=brandjs"),
-                    g = e("<img>").attr("src", "https://d3e54v103j8qbb.cloudfront.net/img/webflow-badge-icon-d2.89e12c322e.svg").attr("alt", "").css({
-                        marginRight: "4px",
-                        width: "26px"
-                    }),
-                    v = e("<img>").attr("src", "https://d3e54v103j8qbb.cloudfront.net/img/webflow-badge-text-d2.c82cec3b78.svg").attr("alt", "Made in Webflow");
-                return A.append(g, v), A[0]
-            }
+            // function h() {
+            //     var A = e('<a class="w-webflow-badge"></a>').attr("href", "https://webflow.com?utm_campaign=brandjs"),
+            //         g = e("<img>").attr("src", "https://d3e54v103j8qbb.cloudfront.net/img/webflow-badge-icon-d2.89e12c322e.svg").attr("alt", "").css({
+            //             marginRight: "4px",
+            //             width: "26px"
+            //         }),
+            //         v = e("<img>").attr("src", "https://d3e54v103j8qbb.cloudfront.net/img/webflow-badge-text-d2.c82cec3b78.svg").attr("alt", "Made in Webflow");
+            //     return A.append(g, v), A[0]
+            // }
 
             function d() {
                 var A = i.children(o),
